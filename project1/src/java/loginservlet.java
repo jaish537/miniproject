28fb11d7603aab57a8c00af739486758e3db73f1
jaish <jaishghati537@gmail.com>
2026-09-18 18:40:55 +0530

    add login jsp and servlet

---
+++ project1/src/java/loginservlet.java
@@ -0,0 +1,27 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class loginservlet extends HttpServlet {
+
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String email=request.getParameter("email");
+        String password=request.getParameter("password");
+        
+        out.println("<h1>login servlet working</h1>");
+        out.println("<p> Email: " +email+"</p>");
+        
+        
+        }
+    }
+
