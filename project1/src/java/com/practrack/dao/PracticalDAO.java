7715afc3a16b448b64b43df026e172af1846cd5d
jaish <jaishghati537@gmail.com>
2026-09-22 16:17:52 +0530

    fix practical dao

--- project1/src/java/com/practrack/dao/PracticalDAO.java
+++ project1/src/java/com/practrack/dao/PracticalDAO.java
@@ -1,10 +1,38 @@
-
 package com.practrack.dao;
 
-/**
- *
- * @author Admin
- */
+import com.practrack.util.dbconnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class PracticalDAO {
-    
+
+    public ResultSet getAllPracticals()
+            throws SQLException {
+
+        String sql =
+                "SELECT * FROM practicals " +
+                "ORDER BY practical_no";
+
+        Connection con = dbconnection.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+
+        return ps.executeQuery();
+    }
+
+    public ResultSet getPracticalById(long practicalId)
+            throws SQLException {
+
+        String sql =
+                "SELECT * FROM practicals " +
+                "WHERE practical_id = ?";
+
+        Connection con = dbconnection.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+
+        ps.setLong(1, practicalId);
+
+        return ps.executeQuery();
+    }
 }
